74de688c0e1ca2d1c9ff75372575510681359b83
MaPl <mapl@york.moe>
2021-12-02 20:03:47 +0800

    lib: utils/serial: import sunxi-uart from tina sdk
    the d1 contains a uart controller, this commit pick it from tina SDK
    and add it to opensbi to support the d1 platform.
    
    Signed-off-by: MaPl <mapl@york.moe>

--- lib/utils/serial/objects.mk
+++ lib/utils/serial/objects.mk
@@ -2,9 +2,11 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 # Copyright (c) 2019 Western Digital Corporation or its affiliates.
+# Copyright (c) 2021 MaPl <mapl@york.moe>
 #
 # Authors:
 #   Anup Patel <anup.patel@wdc.com>
+#   MaPl <mapl@york.moe>
 #
 
 libsbiutils-objs-y += serial/fdt_serial.o
@@ -15,3 +17,4 @@ libsbiutils-objs-y += serial/fdt_serial_uart8250.o
 libsbiutils-objs-y += serial/shakti-uart.o
 libsbiutils-objs-y += serial/sifive-uart.o
 libsbiutils-objs-y += serial/uart8250.o
+libsbiutils-objs-y += serial/sunxi-uart.o
